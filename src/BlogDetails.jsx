caa1e6571e60c40e50b6eb9b29b541467c6fb246
solnsumei <solnsumei@gmail.com>
2021-02-03 12:25:53 +0100

    Add delete button

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,9 +1,21 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+
 
 const Blogdetails = () => {
   const { id } = useParams();
   const { data: blog, isPending, error } = useFetch(`http://localhost:8000/blogs/${id}`);
+  const history = useHistory();
+
+
+  const handleDelete = () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: 'DELETE'
+    })
+    .then(() => {
+      history.push('/');
+    });
+  }
 
   return (
     <div className="blog-details">
@@ -14,6 +26,7 @@ const Blogdetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
